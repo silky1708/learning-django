6d1e74abae074a1b07178ae1da5a718f065a5205
Silky Singh <silky1708@gmail.com>
2022-02-05 22:15:01 +0530

    simple blog

---
+++ blog/README.md
@@ -0,0 +1,3 @@
+#### A simple blog implementation in Django 
+
+![blog](blog.png)
--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def index(request):
+    allPosts = Post.objects.all()
+    return render(request, 'index.html', {'allPosts': allPosts})
